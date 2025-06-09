8ef2194d637f5b0a9f8db917f75b6f4c07614b00
Sebastien <sebastiendumont1986@gmail.com>
2025-06-09 23:30:39 +0200

    Updated cart icon for plugin menu

--- includes/classes/admin/class-cocart-admin-menus.php
+++ includes/classes/admin/class-cocart-admin-menus.php
@@ -56,7 +56,11 @@ if ( ! class_exists( 'CoCart_Admin_Menus' ) ) {
 				function () {
 					return '';
 				},
-				'dashicons-cart',
+				'data:image/svg+xml;base64,' . base64_encode( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+    <path stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 14h.01M11 14h.01" />
+    <path stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.066 4.75H14.25l-1.566 5.088a2 2 0 0 1-1.911 1.412H6.55a2 2 0 0 1-1.99-1.79l-.623-5.92A2 2 0 0 0 1.95 1.75H1.75" />
+</svg>
+' ), // php:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
 				80
 			);
 		} // END add_main_menu_page()
