d228b697c4f0ca758ab995cbdc1c10016e7c13a4
Sebastien <sebastiendumont1986@gmail.com>
2025-06-16 20:47:45 +0200

    Updated next steps and corrected WC style conflicts

--- includes/classes/admin/views/html-next-steps.php
+++ includes/classes/admin/views/html-next-steps.php
@@ -97,21 +97,38 @@ $campaign_args = CoCart_Helpers::cocart_campaign( // phpcs:ignore: WordPress.Nam
 			<p class="next-step-heading"><?php esc_html_e( 'You can also', 'cart-rest-api-for-woocommerce' ); ?></p>
 		</div>
 		<div class="cocart-next-step-action">
-			<p class="cocart-actions step">
-				<a class="button cocart-button-alt" href="<?php echo esc_url( CoCart_Helpers::build_shortlink( add_query_arg( $campaign_args, esc_url( COCART_STORE_URL . 'community/' ) ) ) ); ?>" target="_blank" rel="noopener noreferrer">
-					<?php esc_html_e( 'Join Community', 'cart-rest-api-for-woocommerce' ); ?>
-				</a>
-				<?php
+			<div class="column">
+				<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 640 512" aria-hidden="true" focusable="false">
+					<path d="M0 128C0 92.7 28.7 64 64 64l192 0 48 0 16 0 256 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64l-256 0-16 0-48 0L64 448c-35.3 0-64-28.7-64-64L0 128zm320 0l0 256 256 0 0-256-256 0zM178.3 175.9c-3.2-7.2-10.4-11.9-18.3-11.9s-15.1 4.7-18.3 11.9l-64 144c-4.5 10.1 .1 21.9 10.2 26.4s21.9-.1 26.4-10.2l8.9-20.1 73.6 0 8.9 20.1c4.5 10.1 16.3 14.6 26.4 10.2s14.6-16.3 10.2-26.4l-64-144zM160 233.2L179 276l-38 0 19-42.8zM448 164c11 0 20 9 20 20l0 4 44 0 16 0c11 0 20 9 20 20s-9 20-20 20l-2 0-1.6 4.5c-8.9 24.4-22.4 46.6-39.6 65.4c.9 .6 1.8 1.1 2.7 1.6l18.9 11.3c9.5 5.7 12.5 18 6.9 27.4s-18 12.5-27.4 6.9l-18.9-11.3c-4.5-2.7-8.8-5.5-13.1-8.5c-10.6 7.5-21.9 14-34 19.4l-3.6 1.6c-10.1 4.5-21.9-.1-26.4-10.2s.1-21.9 10.2-26.4l3.6-1.6c6.4-2.9 12.6-6.1 18.5-9.8l-12.2-12.2c-7.8-7.8-7.8-20.5 0-28.3s20.5-7.8 28.3 0l14.6 14.6 .5 .5c12.4-13.1 22.5-28.3 29.8-45L448 228l-72 0c-11 0-20-9-20-20s9-20 20-20l52 0 0-4c0-11 9-20 20-20z"/>
+				</svg>
+				<h3><?php esc_html_e( 'Translate', 'cart-rest-api-for-woocommerce' ); ?></h3>
+				<p><?php echo sprintf(
+					/* translators: %1$s: Hyperlink opening, %2$s: Hyperlink closing, %3$s: CoCart */
+					__( '%3$s is in need of translations. Is the plugin not translated in your language or do you spot errors with the current translations? Helping out is easy!', 'cart-rest-api-for-woocommerce' ),
+					'<a href="' . esc_url( COCART_TRANSLATION_URL ) . '" aria-label="' . esc_html__( 'Help translate', 'cart-rest-api-for-woocommerce' ) . '" title="' . esc_html__( 'Help translate', 'cart-rest-api-for-woocommerce' ) . ' "target="_blank" rel="noopener noreferrer">',
+					'</a>',
+					'CoCart'
+				); ?></p>
+				<a class="button cocart-button-alt" href="<?php echo esc_url( COCART_TRANSLATION_URL ); ?>" target="_blank"><?php esc_html_e( 'Help translate', 'cart-rest-api-for-woocommerce' ); ?></a>
+			</div>
+			<div class="column">
+				<svg width="48" height="48" id="Discord-Logo" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" viewBox="0 0 126.644 96">
+					<defs><style>.cls-1{fill:#5865f2;}</style></defs>
+					<path id="Discord-Symbol-Blurple" class="cls-1" d="M81.15,0c-1.2376,2.1973-2.3489,4.4704-3.3591,6.794-9.5975-1.4396-19.3718-1.4396-28.9945,0-.985-2.3236-2.1216-4.5967-3.3591-6.794-9.0166,1.5407-17.8059,4.2431-26.1405,8.0568C2.779,32.5304-1.6914,56.3725.5312,79.8863c9.6732,7.1476,20.5083,12.603,32.0505,16.0884,2.6014-3.4854,4.8998-7.1981,6.8698-11.0623-3.738-1.3891-7.3497-3.1318-10.8098-5.1523.9092-.6567,1.7932-1.3386,2.6519-1.9953,20.281,9.547,43.7696,9.547,64.0758,0,.8587.7072,1.7427,1.3891,2.6519,1.9953-3.4601,2.0457-7.0718,3.7632-10.835,5.1776,1.97,3.8642,4.2683,7.5769,6.8698,11.0623,11.5419-3.4854,22.3769-8.9156,32.0509-16.0631,2.626-27.2771-4.496-50.9172-18.817-71.8548C98.9811,4.2684,90.1918,1.5659,81.1752.0505l-.0252-.0505ZM42.2802,65.4144c-6.2383,0-11.4159-5.6575-11.4159-12.6535s4.9755-12.6788,11.3907-12.6788,11.5169,5.708,11.4159,12.6788c-.101,6.9708-5.026,12.6535-11.3907,12.6535ZM84.3576,65.4144c-6.2637,0-11.3907-5.6575-11.3907-12.6535s4.9755-12.6788,11.3907-12.6788,11.4917,5.708,11.3906,12.6788c-.101,6.9708-5.026,12.6535-11.3906,12.6535Z"/>
+				</svg>
+				<h3><?php esc_html_e( 'Community Support', 'cart-rest-api-for-woocommerce' ); ?></h3>
+				<p><?php esc_html_e( 'Join the community on Discord to chat with us and fellow CoCart users.', 'cart-rest-api-for-woocommerce' ); ?></p>
+				<a class="button cocart-button-alt" href="<?php echo esc_url( CoCart_Helpers::build_shortlink( add_query_arg( $campaign_args, esc_url( COCART_STORE_URL . 'community/' ) ) ) ); ?>" target="_blank"><?php esc_html_e( 'Join Community', 'cart-rest-api-for-woocommerce' ); ?></a>
+			</div>
+			<div class="column">
+			<?php
 				// Only show upgrade option if neither CoCart Plus, Pro or above is found.
 				if ( apply_filters( 'cocart_show_upgrade_action_link', true ) ) {
 					?>
 				<a class="button cocart-button-alt" href="<?php echo esc_url( CoCart_Helpers::build_shortlink( add_query_arg( $campaign_args, esc_url( COCART_STORE_URL . 'pricing/' ) ) ) ); ?>" target="_blank" rel="noopener noreferrer">
 						<?php esc_html_e( 'Unlock more features', 'cart-rest-api-for-woocommerce' ); ?>
 				</a><?php } ?>
-				<a class="button cocart-button-alt" href="<?php echo esc_url( 'https://marketplace.visualstudio.com/items?itemName=sebastien-dumont.cocart-vscode' ); ?>" target="_blank" rel="noopener noreferrer">
-					<?php esc_html_e( 'Install VSCode Extension', 'cart-rest-api-for-woocommerce' ); ?>
-				</a>
-			</p>
+			</div>
 		</div>
 	</li>
 </ul>
