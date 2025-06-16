032c66654a925038af0f86e5cff57e6f77391628
Sebastien <sebastiendumont1986@gmail.com>
2025-06-16 17:32:39 +0200

    Fixed CoCart icon SVG for menu

--- includes/classes/admin/class-cocart-admin-menus.php
+++ includes/classes/admin/class-cocart-admin-menus.php
@@ -56,11 +56,7 @@ if ( ! class_exists( 'CoCart_Admin_Menus' ) ) {
 				function () {
 					return '';
 				},
-				'data:image/svg+xml;base64,' . base64_encode( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-    <path stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 14h.01M11 14h.01" />
-    <path stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.066 4.75H14.25l-1.566 5.088a2 2 0 0 1-1.911 1.412H6.55a2 2 0 0 1-1.99-1.79l-.623-5.92A2 2 0 0 0 1.95 1.75H1.75" />
-</svg>
-' ), // php:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+				'data:image/svg+xml;base64,' . base64_encode( '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill-rule="oddeven" version="1.1" viewBox="0 0 16 16"><path transform="scale(.8)" d="m2.1875 1.25a0.93759 0.93759 0 1 0 0 1.875h0.25c0.80673 7.844e-4 1.4661 0.59617 1.5508 1.3984l0.14453 1.373a0.93759 0.93759 0 0 0 0.029297 0.2793l0.60547 5.748c0.18391 1.7418 1.6684 3.0771 3.4199 3.0762h5.2793c1.5049-4.6e-4 2.8408-0.98742 3.2832-2.4258l1.959-6.3613a0.93759 0.93759 0 0 0-0.89648-1.2129h-11.887l-0.070312-0.67383c-0.18371-1.7399-1.6664-3.0745-3.416-3.0762a0.93759 0.93759 0 0 0-0.0019531 0h-0.25zm3.9355 5.625h10.42l-1.584 5.1465c-0.20279 0.65927-0.80243 1.1033-1.4922 1.1035h-5.2793a0.93759 0.93759 0 0 0-0.0019531 0c-0.80767 4.26e-4 -1.4679-0.59524-1.5527-1.3984l-0.50977-4.8516zm1.377 9.375a1.2501 1.2501 0 1 0 0 2.5h0.011719a1.2501 1.2501 0 1 0 0-2.5h-0.011719zm6.25 0a1.2501 1.2501 0 1 0 0 2.5h0.011719a1.2501 1.2501 0 1 0 0-2.5h-0.011719z" style="color-rendering:auto;color:#000000;dominant-baseline:auto;fill-rule:nonzero;fill:#ffffff;font-feature-settings:normal;font-variant-alternates:normal;font-variant-caps:normal;font-variant-ligatures:normal;font-variant-numeric:normal;font-variant-position:normal;image-rendering:auto;isolation:auto;mix-blend-mode:normal;opacity:.94;shape-padding:0;shape-rendering:auto;solid-color:#000000;stroke:none;text-decoration-color:#000000;text-decoration-line:none;text-decoration-style:solid;text-indent:0;text-orientation:mixed;text-transform:none;white-space:normal"/></svg>' ), // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
 				80
 			);
 		} // END add_main_menu_page()
